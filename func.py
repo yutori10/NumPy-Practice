12514495e546c1f0a54ff0674ccbaa19bbc24dd9
yutori10 <yutori1107@gmail.com>
2023-01-01 23:40:07 +0900

    discribe

--- func.py
+++ func.py
@@ -1,7 +1,7 @@
 #import文, モジュールのインポート
 #matplotlib.pyplotはグラフを描画するためのモジュール
 import numpy as np
-import matpoltlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 #独立変数xについて定義, 0~500の長さの横軸を2π毎に区切る
 #np.linspaceは数字を指定した感覚で区切って配列を生成する関数
@@ -11,3 +11,4 @@ x = np.linspace(0, 2*np.pi, 500)
 #xの描画とそれに対応するsinxの描画
 #y = np.sin(x)としてplt.plot(x, y)と書いても良い
 plt.plot(x, np.sin(x))
+#これが基本的な関数の書き方となります
